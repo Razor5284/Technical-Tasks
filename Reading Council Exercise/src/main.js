9a4515645bdc93b3c2584b3ea3bd33380dcabff0
Razor5284 <ryan.work@hotmail.co.uk>
2021-06-30 23:48:44 +0100

    Postcode input returns UPRN, added selectbox for addresses

--- Reading Council Exercise/src/main.js
+++ Reading Council Exercise/src/main.js
@@ -2,23 +2,59 @@
 
   const state = {
     userInput: null,
+    userSelection: null,
     fetchingData: false,
-    fetchedData: null,
+    fetchedPCData: null,
+    fetchedUPRNData: null,
     options: {
-      requestURL: 'https://api.readingdev.com/rbv/getaddresses'
+        postcodeURL: 'https://api.readingdev.com/rbc/getaddresses',
+        collectionsURL: 'https://api.readingdev.com/api/collections'
     },
   };
-  console.log("test");
+  const input = document.getElementById("postcode");
+  const pcButton = document.getElementById("postcodeButton");
+  const uprnButton = document.getElementById("UPRNButton");
+  const selectbox = document.getElementById("UPRNSelectbox")
 
-  function makeRequest() {
+
+  // using on change event in the possibility of scaling logic in future, i.e. live validation
+  // input.addEventListener("change", () => {
+  //   state.userInput = input.value;
+  //   getPostcode();
+  // })
+
+  pcButton.addEventListener("click", () => {
     try {
-      fetch(`${state.options.requestURL}/${state.userInput}`)
-        .then(res => res.json()).then(res => state.fetchedData = res);
-        console.log(res);
-      }
-    catch(e) {
+      state.userInput = input.value;
+      const data = fetch(`${state.options.postcodeURL}/${state.userInput}`)
+        .then(res => res.json())
+        .then(data => state.fetchedPCData = data);
+      if (state.fetchedPCData !== null) outputAddresses();
+    } catch(e) {
       // update ui with error message
     }
+  })
+
+  uprnButton.addEventListener("click", () => {
+    try {
+      state.userSelection = selectbox.value
+      const data = fetch(`${state.options.collectionsURL}/${state.userSelection}`)
+        .then(res => res.json())
+        .then(res => state.fetchedUPRNData = res);
+    } catch(e) {
+      // update ui with error message
+    }
+  })
+
+  function outputAddresses() {
+    document.getElementById("UPRNForm").removeAttribute("hidden")
+
+    state.fetchedPCData.Addresses.forEach((index, i) => {
+      option = document.createElement('option');
+      option.text = index.SiteShortAddress;
+      option.value = index.AccountSiteUprn;
+      selectbox.add(option);
+    });
   }
 
 })();
